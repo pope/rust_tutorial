92058a200a658dc56074447773294e4c0629d987
K. Adam Christensen <pope@shifteleven.com>
2022-12-28 12:40:46 -0800

    Fix typo

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -83,7 +83,7 @@ impl<'a> ImportantExcerpt<'a> {
 	}
 
 	fn announce_and_return_part(&self, announcement: &str) -> &str {
-		println!("Atention please: {}", announcement);
+		println!("Attention please: {}", announcement);
 		self.part
 	}
 }
